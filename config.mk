6f5f229bb584523ed4de34740524eab2254a8a91
Alejandro Gallo <aamsgallo@gmail.com>
2017-10-09 17:34:51 +0200

    Add linking

--- config.mk
+++ config.mk
@@ -3,3 +3,12 @@ PIP = pip3
 TEST_COMMAND = $(PYTHON) -m doctest vpqrsreducer/*.py ; \
 							 $(PYTHON) -m pytest vpqrsreducer
 
+
+.PHONY: cc4s-real
+
+scripts/vpqrsreducer:
+	(cd scripts/ ; ln -s ../vpqrsreducer/ .)
+
+cc4s-real: scripts/vpqrsreducer ## Create real integrals for cc4s
+	$(PYTHON) scripts/cc4s-real.py
+
